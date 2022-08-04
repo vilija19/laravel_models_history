56c73a68ede73691b6ba0176cb4e20e07264ad83
Alex <billibons777@gmail.com>
2022-08-04 15:34:42 +0000

    Added skeleton

---
+++ src/ModelHistoryServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class ModelHistoryServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        # code...
+    }
+    
+}
